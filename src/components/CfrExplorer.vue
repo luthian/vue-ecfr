acbae914a032f1ed3253f9a1ce5ad927b7d36ef5
DDA <github@dantonio.net>
2025-03-01 15:37:59 -0500

    Added counting corrections
    - Also added spinners for loading data using async/await to avoid too many requests at once
    - Added switch to only show agencies with corrections
    - Search children for corrections and apply those to the parent agency. Apparently, they all get applied to “office of the secretary of the [Agency]
    Added caching of title data

--- src/components/CfrExplorer.vue
+++ src/components/CfrExplorer.vue
@@ -6,26 +6,27 @@ export default {
     return {
       //
       isLoading: false,
+      isLoadingCorrections: false,
+      onlyShowCorrections: false,
       agencies: [],
       selectedAgencies: [],
       cfrData: [],
       correctionsCountPerAgency: [],
       agencyLetter: '',
       alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      titleCache: [],
     };
   },
   methods: {
     getCfrData() {
       // Fetch the CFR data from the API
-      const CfrServer = 'https://www.ecfr.gov/api/admin/v1/';
-      const url = `${CfrServer}agencies.json`;
-      this.fetchData(url, {})
+      const url = 'agencies.json';
+      this.fetchData('admin', '1', url, {})
         .then(response => response.json())
         .then(data => {
           this.cfrData = data;
-          this.agencies = this.cfrData.agencies.sort((a, b) => a.display_name.localeCompare(b.display_name));
-          this.selectedAgencyIndex = 0;
-          // this.getCorrectionsCountPerAgency();
+          this.agencies = this.cfrData.agencies.sort((a, b) => a.sortable_name.localeCompare(b.sortable_name));
+          this.getCorrectionsCountPerAgency();
         })
         .catch(error => {
           console.error('Error fetching agency data:', error);
@@ -34,29 +35,76 @@ export default {
           this.isLoading = false;
         });
     },
-    getCorrectionsCountPerAgency() {
+    async getCorrectionsCountPerAgency() {
+      this.isLoadingCorrections = true;
       // Get the corrections count for each agency. To do this, we need to iterate through all the children, if any, of each agency.
-      // There aren't any children of children, so we can just get the word count of each part.
-      this.correctionsCountPerAgency = this.agencies.map(agency => {
-        agency.cfr_references.forEach(item => {
-          const url = `${agency.url}corrections/title/${item.title}.json`;
-          this.fetchData(url, {})
-            .then(response => response.json())
-            .then(data => {
-              const wordCount = data.parts.reduce((acc, part) => acc + part.word_count, 0);
-              return {
-                agency: agency.display_name,
-                count: wordCount,
-              };
-            })
-            .finally(() => {
-              this.isLoading = false;
-            });
+      // const allChildren = this.agencies.reduce((acc, agency) => acc.concat(agency.children), []);
+      // const allAgencies = this.agencies.concat(allChildren);
+      this.correctionsCountPerAgency.splice(0);
+      // Use a for loop because we need to await the response
+      for (let i = 0; i < this.agencies.length; i++) {
+        const agency = this.agencies[i];
+        let totalCorrections = await this.countCorrectionsForAgency(agency);
+        // There aren't any children of children, so we can just get the corrections for each child.
+        if (agency.children.length > 0) {
+          for (let j = 0; j < agency.children.length; j++) {
+            const child = agency.children[j];
+            const childCorrections = await this.countCorrectionsForAgency(child);
+            if (childCorrections > 0) {
+              this.correctionsCountPerAgency.push({
+                agency: child.name,
+                count: childCorrections,
+              });
+              // console.log(child.name, childCorrections);
+            }
+            // Add the child's corrections to the parent's total
+            totalCorrections += childCorrections;
+          }
+        }
+        if (totalCorrections > 0) {
+          this.correctionsCountPerAgency.push({
+            agency: agency.name,
+            count: totalCorrections,
+          });
+        }
+      }
+      this.isLoadingCorrections = false;
+      // console.log(this.correctionsCountPerAgency, this.titleCache);
+    },
+    async countCorrectionsForAgency(agency) {
+      let agencyCorrections = 0;
+      // We need a for loop here because we need to await the response
+      for (let i = 0; i < agency.cfr_references.length; i++) {
+        const item = agency.cfr_references[i];
+        const numTitle = Number(item.title);
+        // Arrays need a number so we convert the title to a number
+        if (!this.titleCache[numTitle]) {
+          try {
+            const url = `corrections/title/${item.title}.json`;
+            const res = await this.fetchData('admin', '1', url, {});
+            const response = await res.json();
+            // Stuff it in the cache
+            this.titleCache[numTitle] = response;
+          } catch (error) {
+            console.error('Error fetching corrections data:', error);
+          }
+        }
+        // Now get the data out of the cache
+        const data = this.titleCache[numTitle];
+        let localCorrections = 0;
+        data.ecfr_corrections.forEach(correction => {
+          // This is incomplete because not all corrections have a chapter
+          // But there is no way to get the chapter from the data in the correction
+          if (correction.cfr_references.hierachy?.chapter === item.chapter) {
+            localCorrections += 1;
+          }
         });
-      });
+        agencyCorrections += localCorrections;
+      }
+      return agencyCorrections;
     },
     filteredAgenciesCount(letter) {
-      const filtered = this.agencies.filter(agency => agency.display_name.startsWith(letter));
+      const filtered = this.agencies.filter(agency => agency.name.startsWith(letter));
       return filtered.reduce((acc, agency) => acc + agency.children.length + 1, 0);
     },
     pluralize(count, singular, plural) {
@@ -65,8 +113,10 @@ export default {
       }
       return count === 1 ? singular : plural;
     },
-    fetchData(url, params) {
-      return fetch(url, {
+    fetchData(type, version, url, params) {
+      const CfrServer = `https://www.ecfr.gov/api/${type}/v${version}/`;
+      const fullUrl = `${CfrServer}${url}`;
+      return fetch(fullUrl, {
         method: 'GET',
         mode: 'cors',
         headers: {
@@ -78,16 +128,28 @@ export default {
         ...params,
       });
     },
+    agencyHasCorrections(agencyName) {
+      return this.correctionsCountPerAgency.some(agency => agency.agency === agencyName);
+    },
+    getCorrectionsForAgency(agencyName) {
+      return this.correctionsCountPerAgency.find(agency => agency.agency === agencyName).count;
+    },
   },
   computed: {
     agencyCount() {
-      return this.filteredAgencies.reduce((acc, agency) => acc + agency.children.length + 1, 0);
+      return this.agencies.reduce((acc, agency) => acc + agency.children.length + 1, 0);
     },
     filteredAgencies() {
       if (!this.agencyLetter) {
-        return this.agencies;
+        return this.agencies.filter(agency =>
+          this.onlyShowCorrections ? this.agencyHasCorrections(agency.name) : true
+        );
       }
-      return this.agencies.filter(agency => agency.display_name.startsWith(this.agencyLetter));
+      return this.agencies.filter(
+        agency =>
+          agency.name.startsWith(this.agencyLetter) &&
+          (this.onlyShowCorrections ? this.agencyHasCorrections(agency.name) : true)
+      );
     },
   },
   created() {
@@ -104,6 +166,22 @@ export default {
     <v-row>
       <v-col>
         <h2 class="text-center">{{ agencyCount }} Federal Agencies &#128561;</h2>
+        <div v-if="isLoading" class="text-center">
+          <v-progress-circular indeterminate size="16" color="blue" width="2" class="mr-1"></v-progress-circular>
+          Loading agency data...
+        </div>
+        <div v-else-if="agencies.length === 0" class="text-center">
+          <p>No agencies found.</p>
+        </div>
+        <div v-else-if="isLoadingCorrections" class="text-center">
+          <v-progress-circular indeterminate size="16" color="blue" width="2" class="mr-1"></v-progress-circular>
+          Loading corrections data...
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align-self="end">
+        <v-switch v-model="onlyShowCorrections" label="Only show agencies with corrections"></v-switch>
       </v-col>
     </v-row>
     <v-row>
@@ -140,22 +218,27 @@ export default {
           item-text="name"
           item-key="slug"
           label="Agency tree"
-          selectable
           return-object
-        />
+        >
+          <template v-slot:prepend="{ item, open }">
+            <v-icon v-if="item.children && item.children.length" @click="open ? $emit('close') : $emit('open')">
+              {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+            </v-icon>
+            <span v-else>&#127970;</span>
+          </template>
+          <template v-slot:label="{ item }">
+            {{ item.name }}
+            <span v-if="item.children && item.children.length" class="text-caption">
+              ({{ item.children.length }} {{ pluralize(item.children.length, 'subagency', 'subagencies') }})
+            </span>
+            <div v-if="!isLoadingCorrections && agencyHasCorrections(item.name)" class="text-caption">
+              This agency has {{ getCorrectionsForAgency(item.name) }}
+              {{ pluralize(getCorrectionsForAgency[item.name], 'correction', 'corrections') }}.
+            </div>
+          </template>
+        </v-treeview>
+        {{ correctionsCountPerAgency }}
       </v-col>
-      <!-- <v-col>
-        <v-btn @click="getCorrectionsCountPerAgency">Get Word Count</v-btn>
-      </v-col>
-      <v-col>
-        <v-list>
-          <v-list-item-group>
-            <v-list-item v-for="(agency, index) in correctionsCountPerAgency" :key="index">
-              <v-list-item-title>{{ agency.agency }}: {{ agency.count }}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-col> -->
     </v-row>
   </v-container>
 </template>
